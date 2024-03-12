95d025d591e95e6b8b356fa13b997b083c1a9ad1
Nahu258 <milani.kalitko.3012@gmail.com>
2024-03-11 21:43:12 -0300

    fix: navbar styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <main className="">
+    <main>
       Home sweet home
     </main>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,22 @@
+'use client'
 import Link from 'next/link'
+import { useState } from 'react'
 
 function Header() {
+  const [mobileNavActive, setMobileNavActive] = useState(false)
+
   return (
-    <header className='bg-gray-900'>
+    <header className='bg-[#222]'>
       <div className='max-w-[880px] mx-auto px-5'>
-        <div className='flex justify-between py-5'>
-          <Link href='/' className='block text-white text-xl md:text-base font-bold py-2'>Ecommerce</Link>
-          <nav className='hidden md:flex flex-col md:flex-row gap-4'>
+        <div className='flex justify-between py-5 px-0'>
+          <Link href='/' className='text-white no-underline text-xl font-bold relative z-3'>Ecommerce</Link>
+          <nav className={`${mobileNavActive ? 'block' : 'hidden'} gap-4 fixed top-0 bottom-0 left-0 right-0 py-[70px] px-[20px] pb-[20px] md:p-0 bg-[#222]`}>
             <Link href='/' className='nav-link'>Home</Link>
             <Link href='/products' className='nav-link'>All Products</Link>
             <Link href='/categories' className='nav-link'>Categories</Link>
             <Link href='/cart' className='nav-link'>Cart (0)</Link>
           </nav>
-          <button className='bg-transparent w-10 h-10 md:hidden text-white cursor-pointer'>
+          <button className='bg-transparent w-8 h-8 border-0 text-white cursor-pointer relative z-3 md:hidden' onClick={() => setMobileNavActive(!mobileNavActive)}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
             </svg>
